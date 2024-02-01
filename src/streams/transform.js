a203599fc890fbf8145f416b02826c787ffa9f24
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2024-02-02 02:59:16 +0300

    feat: implement stream functions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { Transform } from 'stream';
+
+const reverseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    const reversedText = chunk.toString().split('').reverse().join('') + '\n';
+    this.push(reversedText);
+    callback();
+  }
+});
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+  reverseTransform.on('finish', () => {
+    console.log('Text transformation completed.');
+  });
+
+  reverseTransform.on('error', (err) => {
+    console.error('Error during text transformation:', err);
+  });
 };
 
 await transform();
