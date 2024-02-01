e2bf467ddd33f01570bcb7da90b8137bcbe2950b
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2024-02-02 02:23:29 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { createHash } from "crypto";
+import { createReadStream } from 'node:fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash("sha256");
+    const input = createReadStream(filePath, 'utf-8');
+
+    return new Promise((resolve, reject) => {
+        input.on('data', (data) => {
+            hash.update(data);
+        });
+
+        input.on('end', () => {
+            console.log(`${hash.digest('hex')}`);
+            resolve();
+        });
+
+        input.on('error', (error) => {
+            reject(error);
+        });
+    });
 };
 
 await calculateHash();
