e5f5ea769df5579e0e984e49050214d2f5893f60
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2024-02-02 23:15:24 +0300

    feat: implement ws task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
+import { Worker } from "worker_threads";
+import * as os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const numberOfCpuCores = os.cpus().length;
+  console.log(numberOfCpuCores)
+  const workers = Array.from({ length: numberOfCpuCores }, (_, index) => {
+    const worker = new Worker("./worker.js", { workerData: index + 10 });
+    return new Promise((resolve) => {
+      worker.on("message", (result) => resolve(result));
+    });
+  });
+
+  const results = await Promise.all(workers);
+
+  console.log("Results:", results);
 };
 
 await performCalculations();
