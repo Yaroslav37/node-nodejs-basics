514cc8190bd91485d47bd04e5056c33060fd05cb
Minenkov Yaroslav <minenkovyaroslav@mail.ru>
2024-02-02 18:23:28 +0300

    feat: implement compress/decompress using zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import * as zlib from "node:zlib";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
 const decompress = async () => {
-    // Write your code here 
+  const sourceFilePath = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "archive.txt.gz"
+  );
+
+  const destinationFilePath = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "files",
+    "fileToCompress.txt"
+  );
+  fs.createReadStream(sourceFilePath)
+    .pipe(zlib.createGunzip())
+    .pipe(fs.createWriteStream(destinationFilePath));
 };
 
 await decompress();
