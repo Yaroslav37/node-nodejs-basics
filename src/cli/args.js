2b40a4400d3f246bf03e93284f2e5189e84f43a9
Yaroslav <minenkovyaroslav@mail.ru>
2024-06-23 00:18:22 +0300

    feat: implement module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,15 +1,17 @@
 const parseArgs = () => {
-    const elements = process.argv.slice(2);
-    const output = elements.map((element, index) => {
-        if (index % 2 === 0){
-            return `${element.slice(2)} is ${elements[index + 1]}`;
-        }
-    }).filter(Boolean);
-    // let output = [];
-    // for (let i = 0; i < elements.length; i+=2){
-    //     output.push(`${elements[i].slice(2)} is ${elements[i+1]}`);
-    // }
-    console.log(output.join(", "));
+  const elements = process.argv.slice(2);
+  const output = elements
+    .map((element, index) => {
+      if (index % 2 === 0) {
+        return `${element.slice(2)} is ${elements[index + 1]}`;
+      }
+    })
+    .filter(Boolean);
+  // let output = [];
+  // for (let i = 0; i < elements.length; i+=2){
+  //     output.push(`${elements[i].slice(2)} is ${elements[i+1]}`);
+  // }
+  console.log(output.join(", "));
 };
 
 parseArgs();
